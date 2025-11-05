03cc435c88efb503fbee1649a55d5fb820546744
Beatriz Siqueira <siqueirabiaf@gmail.com>
2025-11-05 18:49:24 -0300

    conexão do banco mudada para o aiven

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const db = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
     database: process.env.DATABASE
 });
 
